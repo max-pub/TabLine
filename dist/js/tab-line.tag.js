63be3dca23d4e9e556ff4c5e2e3a8736596f3efd
Max Fechner <max.fechner@gmail.com>
2021-12-31 10:44:31 +0100

    ...

--- dist/js/tab-line.tag.js
+++ dist/js/tab-line.tag.js
@@ -1,22 +1,28 @@
 console.log('tab-line', import.meta.url);
-
-
-//[ HTML
+function NODE(name, attributes = {}, children = []) {
+	let node = document.createElement(name);
+	for (let key in attributes)
+		node.setAttribute(key, attributes[key]);
+	for (let child of children)
+		node.appendChild(typeof child == 'string' ? document.createTextNode(child) : child);
+	return node;
+}
+class XML {
+	static parse(string, type = 'xml') {
+		return new DOMParser().parseFromString(string.replace(/xmlns=".*?"/g, ''), 'text/' + type)
+	}
+	static stringify(DOM) {
+		return new XMLSerializer().serializeToString(DOM).replace(/xmlns=".*?"/g, '')
+	}
+}
+XMLDocument.prototype.stringify = XML.stringify
+Element.prototype.stringify = XML.stringify
 const HTML = document.createElement('template');
 HTML.innerHTML = `<main></main>`;
-// console.log("HTML", HTML);
-//] HTML
-
-
-
-
-
-//[ CSS
 let STYLE = document.createElement('style');
 STYLE.appendChild(document.createTextNode(`/* @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap'); */
 	/* @import url('https://max.pub/css/quicksand.css');
 	@import url('https://max.pub/css/inconsolata.css'); */
-
 	:host {
 		display: inline-block;
 		background: #333;
@@ -25,61 +31,47 @@ STYLE.appendChild(document.createTextNode(`/* @import url('https://fonts.googlea
 		font-size: 14px;
 		white-space: pre;
 		color: white;
-
 		font-family: var(--font, "Lucida Console", Monaco, monospace);
 		font-weight: 100;
 		/* padding: .3rem; */
 	}
-
 	table {
 		width: 100%
 	}
-
 	line {
 		display: block;
 	}
-
 	[level='0'] {
 		/* border-top: 1px solid red; */
 	}
-
 	[level='1'] td:first-child {
 		padding-left: 2rem;
 	}
-
 	[level='2'] td:first-child {
 		padding-left: 4rem;
 	}
-
 	[level='3'] td:first-child {
 		padding-left: 6rem;
 	}
-
 	* {
 		/* font-family: Inconsolata; */
 		font-weight: 100;
 		font-size: 1.1rem;
 	}
-
 	table {
 		border-collapse: collapse;
 	}
-
 	tr:nth-child(even) {
 		background: #393939;
 	}
-
 	:host(:not(.definition)) .type {
 		display: none;
 	}
-
 	/* iframe {
 		width: 100%;
 		height: 100%;
 		border: none;
 	} */
-
-
 	/* body {
 		tab-size: 4;
 		-moz-tab-size: 4;
@@ -88,255 +80,130 @@ STYLE.appendChild(document.createTextNode(`/* @import url('https://fonts.googlea
 		color: white;
 		font-family: monospace;
 	} */
-
-	key {
+	.key {
 		color: #ddd;
 		font-weight: 300;
 	}
-
 	.value {
 		color: #ffa;
 	}
-
 	.cardinality {
 		color: #faa;
 		padding-left: 3rem;
 	}
-
 	.type {
 		color: #aaf;
 		padding-left: 3rem;
 	}
-
 	.constraint {
 		color: #afa;
 		padding-left: 3rem;
 	}
-
-
-
 	.value,
 	.type {
 		color: gray;
 		padding-left: 3rem;
 	}
-
-
 	.string {
 		color: gold;
 	}
-
 	.letters {
 		color: goldenrod;
 	}
-
-
-
 	.date,
 	.time,
 	.datetime {
 		color: magenta;
 	}
-
 	.url,
 	.email,
 	.phone {
 		color: pink;
 	}
-
 	.email {
 		color: papayawhip;
 	}
-
-
 	.bool.false {
 		color: #f44;
 	}
-
 	.bool.true {
 		color: #4f4;
 	}
-
-
-
 	.int {
 		color: aqua;
 	}
-
 	.real {
 		color: aquamarine;
 	}
-
-
-
 	.term {
 		color: pink
 	}
-
 	.open {
 		color: #afa;
 	}
-
 	.close {
 		color: #faa;
 	}
-
 	.validation{color: #f55; font-weight: bold;}`));
-//] CSS
-
-
-
-
-
-// import tabLine from '../../js/base.js';
-	import parse from '../../js/parse.js';
-	import html from '../../js/html.js';
-	import validate from '../../js/validate.js';
-
-
+function QQ(query, i) {
+	let result = Array.from(this.querySelectorAll(query));
+	return i ? result?.[i - 1] : result;
+}
+Element.prototype.Q = QQ
+ShadowRoot.prototype.Q = QQ
+DocumentFragment.prototype.Q = QQ
 class WebTag extends HTMLElement {
-
 	constructor() {
 		super();
-		// console.log('constructor', this.innerHTML);
 		this.attachShadow({ mode: 'open', delegatesFocus: true });
 		this.shadowRoot.appendChild(STYLE.cloneNode(true)); //: CSS
 		this.$HTM = document.createElement('htm')
 		this.shadowRoot.appendChild(this.$HTM)
-		this.$viewUpdateCount = 0;
-
-
 	}
-
-
 	async connectedCallback() {
-
 		this.$applyHTML(); //: HTML
-
 		this.$attachMutationObservers();
 		this.$attachEventListeners();
-
-
-
-
 		this.$onReady(); //: onReady
 	}
-
-
 	$attachMutationObservers() {
-		//[XSLT
 		this.modelObserver = new MutationObserver(events => {
-			// console.log('model change', events, events[0].type, events[0].target, events[0].target == this)
 			if ((events[0].type == 'attributes') && (events[0].target == this)) {
-				
 			} else {
-				this.$onModelChange(events); //: $onModelChange
-
 			}
-
 		}).observe(this, { attributes: true, characterData: true, attributeOldValue: true, childList: true, subtree: true });
-		//] XSLT
-
-		
-
 	}
-	// window.addEventListener('load', () => this.applyXSLT());
-
-	//[x  on-tap  on-key  $onSlotChange
 	$attachEventListeners() {
 		let action = (event, key) => {
 			try {
 				let target = event.composedPath()[0];
-				// let target = event.target;
 				let action = target.closest(`[${key}]`);
-				// console.log('EEE', key, event.composedPath(), target, action, 'called by', this, event)
-				// console.log('PATH', event.composedPath().map(x => this.$1(x)))
 				this[action.getAttribute(key)](action, event, target)
 			}
-			catch  { }
+			catch { }
 		}
-
-
-
-
-
-
-
-
 	}
-	//]  on-tap  on-key  $onSlotChange
-
-
-	//[ HTML
 	$applyHTML() {
-		// this.shadowRoot.innerHTML = `<style>${STYLE.textContent}</style>` + new XMLSerializer().serializeToString(HTML);
 		this.$view = HTML.content.cloneNode(true)
-		// 	this.$clearView();
-		// this.shadowRoot.appendChild(STYLE.cloneNode(true));
-		// this.shadowRoot.appendChild(HTML.content.cloneNode(true));
-		// this.shadowRoot.insertAdjacentElement('afterbegin',STYLE);
 	}
-	//] HTML
-
-
-
-	// $clearView() {
-	// 	this.$clear(this.shadowRoot);
-	// }
 	$clear(R) {
-		// https://jsperf.com/innerhtml-vs-removechild/15  >> 3 times faster
 		while (R.lastChild)
 			R.removeChild(R.lastChild);
 	}
-
-
-	// set $style(HTML) {
-	// 	this.shadowRoot.innerHTML = HTML;
-	// }
 	get $view() {
 		return this.$HTM;
-		// return this.shadowRoot.lastChild;
 	}
 	set $view(HTML) {
 		this.$clear(this.$view);
+		if (typeof HTML == 'string')
+			HTML = new DOMParser().parseFromString(HTML, 'text/html').firstChild
 		this.$view.appendChild(HTML);
 	}
-
-	
-
-
-	// 	let treeWalker = document.createTreeWalker(temp1, NodeFilter.SHOW_ELEMENT);
-	// let node = null;
-	// let list = [];
-	// while (node = treeWalker.nextNode()) {
-	// 	list.push(currentNode)
-	// }
-
-
-
-
-
-
-
-
-	$q1(q) { return this.shadowRoot.querySelector(q) } //: viewQS1
-
-
-
-	
-
-
-	
-
-
-
-	
-
-
-	//--------------------------------------------
-	//--------------------------------------------
-
-	
+};
+	import {parse1 as parse} from '../../js/parse.js';
+	import html from '../../js/html.js';
+	import validate from '../../js/validate.js';
+	class tab_line extends WebTag {
 		$onReady() {
 			this.show()
 		}
@@ -349,7 +216,7 @@ class WebTag extends HTMLElement {
 				let type = '';
 				if (this.classList.contains('definition')) type = 'definition'
 				if (this.classList.contains('transformation')) type = 'transformation'
-				let data = parse(this.textContent, type);
+				let data = parse(this.textContent, type).root;
 				let definition = this.getAttribute('definition');
 				if (definition) {
 					definition = await fetch(definition).then(x => x.text());
@@ -357,24 +224,8 @@ class WebTag extends HTMLElement {
 					definition = parse(definition, 'definition')
 					validate(data, definition);
 				}
-				// if (this.classList.contains('definition'))
-				// 	data = tabLine.parseDefinition(data)
-
-				// console.log('data', data);
-				// console.log('html', html(data))
-				this.$q1('main').innerHTML = '<table>' + html(data) + '</table>';
-				// console.log('html', tabLine.toHTML(data))
-				// console.log('json',tabLine.toJSON(data))
-				// console.log('json', tabLine.toTypeScript(data))
-				// window.tal = tabLine.parse(this.textContent);
-				// window.tas = tabLine.stringify(window.tal);
-				// console.log(tal, tas);
+				this.$view.Q('main',1).innerHTML = '<table>' + html(data) + '</table>';
 			} catch (e) { console.log('error', e) }
 		}
-
-
-};
-// console.log(WebTag)
-window.customElements.define('tab-line', WebTag)
-
-
+	}
+window.customElements.define('tab-line', tab_line)
